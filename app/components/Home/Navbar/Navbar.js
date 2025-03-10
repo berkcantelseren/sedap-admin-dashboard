e6d0ce9e1876eb6310f93de4650fc3c4259378db
Berkcan Telseren <berkcantelseren@gmail.com>
2025-03-10 21:34:21 +0300

    finished top navbar and added focus and scale for icons

--- app/components/Home/Navbar/Navbar.js
+++ app/components/Home/Navbar/Navbar.js
@@ -3,11 +3,12 @@ import { GoBellFill } from "react-icons/go";
 import { RiMessage2Line } from "react-icons/ri";
 import { SlPresent } from "react-icons/sl";
 import { IoSettingsOutline } from "react-icons/io5";
+import Image from "next/image";
 
 export default function Navbar() {
   return (
-    <div className="w-[80%] fixed ml-[55px] justify-items-center h-24">
-      <div>
+    <div className="w-[75%] fixed ml-[65px] justify-items-center h-24">
+      <div className="group">
         {/* search section  */}
         <input
           type="text"
@@ -15,27 +16,46 @@ export default function Navbar() {
           className="w-[41rem] h-[2.5rem] rounded-lg text-gray-500 align-center justify-center flex ml-20 mt-10 text-left p-5 border border-gray-300 outline-0"
         />
         <div className="flex justify-end">
-          <CgSearch className="w-6 h-6 text-gray-400 mt-[-31px] mr-[12px]" />
+          <CgSearch className="w-6 h-6 text-gray-400 mt-[-31px] mr-[12px] transition-transform group-focus-within:scale-125" />
         </div>
       </div>
       {/* icons section  */}
       <div className="justify-center items-center flex w-[250px] ml-250 mt-[-50] h-15">
         {/* right line  */}
-        <div className="absolute right-[-85px] h-[60px] w-[2px] bg-gray-300"></div>
+        <div className="absolute right-[-110px] h-[60px] w-[2px] bg-gray-300"></div>
 
         <div className="grid grid-cols-4 gap-15 p-5">
-          <div className="bg-blue-300/40 rounded-xl p-4 mx-auto cursor-pointer ">
-            <GoBellFill className="w-6 h-6 text-blue-400 hover:scale-120" />
+          <div className="bg-blue-300/40 rounded-xl p-4 mx-auto cursor-pointer group">
+            <GoBellFill className="w-6 h-6 text-blue-400 transition-transform group-hover:scale-120" />
           </div>
-          <div className="bg-blue-300/40 rounded-xl p-4 mx-auto cursor-pointer ">
-            <RiMessage2Line className="w-6 h-6 text-blue-400 hover:scale-120" />
+          <div className="bg-blue-300/40 rounded-xl p-4 mx-auto cursor-pointer group">
+            <RiMessage2Line className="w-6 h-6 text-blue-400 transition-transform group-hover:scale-120" />
           </div>
-          <div className="bg-purple-900/25 rounded-xl p-4 mx-auto cursor-pointer">
-            <SlPresent className="w-6 h-6 text-[#5e6c93] hover:scale-120" />
+          <div className="bg-purple-900/25 rounded-xl p-4 mx-auto cursor-pointer group">
+            <SlPresent className="w-6 h-6 text-[#5e6c93] transition-transform group-hover:scale-120" />
           </div>
-          <div className="bg-rose-500/25 rounded-xl p-4 mx-auto cursor-pointer">
-            <IoSettingsOutline className="w-6 h-6 text-rose-500 hover:scale-120" />
+          <div className="bg-rose-500/25 rounded-xl p-4 mx-auto cursor-pointer group">
+            <IoSettingsOutline className="w-6 h-6 text-rose-500 transition-transform group-hover:scale-120" />
           </div>
+        </div>
+      </div>
+      {/* profile section */}
+      <div className="justify-start items-center flex w-[180px] ml-390 mt-[-60] h-15 text-start">
+        <div className="ml-3">
+          <p>
+            <span className="text-sm text-gray-600 mr-1">Hello,</span>
+            <span className="font-bold text-md text-gray-700">Berkcan</span>
+          </p>
+        </div>
+        {/* image section  */}
+        <div>
+          <Image
+            src="/images/pp.jpg"
+            alt="profile picture"
+            width={50}
+            height={50}
+            className="rounded-3xl ml-3 select-none cursor-pointer"
+          />
         </div>
       </div>
     </div>
