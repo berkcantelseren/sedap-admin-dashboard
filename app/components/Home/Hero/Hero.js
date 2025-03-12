0662aa71823e14724c776dc901d7b71c3a3c2682
Berkcan Telseren <berkcantelseren@gmail.com>
2025-03-12 23:03:48 +0300

    hero text section classes finished

--- app/components/Home/Hero/Hero.js
+++ app/components/Home/Hero/Hero.js
@@ -7,9 +7,12 @@ const BarlowFont = Barlow({
 
 export default function Hero() {
   return (
-    <div className="ml-[345px] -mt-[575px]">
-      <h1 className="font-bold text-2xl">Dashboard</h1>
-      <p>Hi, Berkcan. Welcome back to Sedap Admin!</p>
+    <div className="ml-[350px] -mt-[575px]">
+      {/* text section  */}
+      <h1 className={` ${BarlowFont.className} font-bold text-2xl`}>
+        Dashboard
+      </h1>
+      <p className="text-gray-400">Hi, Berkcan. Welcome back to Sedap Admin!</p>
     </div>
   );
 }
