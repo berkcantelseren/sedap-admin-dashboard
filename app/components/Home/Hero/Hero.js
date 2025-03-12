75851a17d46dbb7544734300dc91db0106e9f373
Berkcan Telseren <berkcantelseren@gmail.com>
2025-03-12 23:16:56 +0300

    icons added for delivery section

--- app/components/Home/Hero/Hero.js
+++ app/components/Home/Hero/Hero.js
@@ -1,4 +1,5 @@
 import { Barlow } from "next/font/google";
+import Image from "next/image";
 
 const BarlowFont = Barlow({
   weight: "600",
@@ -13,6 +14,12 @@ export default function Hero() {
         Dashboard
       </h1>
       <p className="text-gray-400">Hi, Berkcan. Welcome back to Sedap Admin!</p>
+      <div className="grid grid-cols-4 mt-20 ml-30">
+        <div></div>
+        <div>2</div>
+        <div>3</div>
+        <div>4</div>
+      </div>
     </div>
   );
 }
