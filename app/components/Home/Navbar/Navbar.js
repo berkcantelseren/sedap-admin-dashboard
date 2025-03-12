2dac8dbcf3d4c22e2adf8d57dbb7c4dc0b907986
Berkcan Telseren <berkcantelseren@gmail.com>
2025-03-13 00:42:39 +0300

    fixed navbar bugs and background issues (fixes are so bad I dont even know how it works.)

--- app/components/Home/Navbar/Navbar.js
+++ app/components/Home/Navbar/Navbar.js
@@ -20,10 +20,9 @@ export default function Navbar() {
         </div>
       </div>
       {/* icons section  */}
-      <div className="justify-center items-center flex w-[250px] ml-250 mt-[-50] h-15">
+      <div className="justify-center items-center flex w-[250px] ml-250 mt-[-50] h-15 bg-white z-20">
         {/* right line  */}
-        <div className="absolute right-[-110px] h-[60px] w-[2px] bg-gray-300"></div>
-
+        <div className="absolute right-[-110px] h-[60px] w-[2px] bg-gray-300 z-50"></div>
         <div className="grid grid-cols-4 gap-15 p-5">
           <div className="bg-blue-300/40 rounded-xl p-4 mx-auto cursor-pointer group">
             <GoBellFill className="w-6 h-6 text-blue-400 transition-transform group-hover:scale-120" />
@@ -43,19 +42,24 @@ export default function Navbar() {
               15
             </div>
           </div>
-          <div className="bg-rose-500/25 rounded-xl p-4 mx-auto cursor-pointer group">
+          <div className="bg-rose-500/25 rounded-xl p-4 mx-auto cursor-pointer group z-50">
             <IoSettingsOutline className="w-6 h-6 text-rose-500 transition-transform group-hover:scale-120" />
             <div className="absolute top-0 right-0 w-5 h-5 bg-rose-400 text-white text-xs rounded-full flex items-center justify-center -mr-22 mt-7">
               19
             </div>
           </div>
         </div>
+        {/* bg white  */}
+        <div className="fixed w-20 h-15 pt-40 mb-25 bg-white ml-80 z-0"></div>
       </div>
+
       {/* profile section */}
-      <div className="justify-start items-center flex w-[180px] ml-390 mt-[-60] h-15 text-start">
+      <div className="justify-start items-center flex w-[180px] ml-390 mt-[-60] h-15 text-start bg-white relative">
+        {/* bg white  */}
+        <div className="absolute w-50 h-[40px] mb-24 bg-white"></div>
         <div className="ml-3">
           <p>
-            <span className="text-sm text-gray-600 mr-1">Hello,</span>
+            <span className="text-sm text-gray-600 mr-1 z-50">Hello,</span>
             <span className="font-bold text-md text-gray-700">Berkcan</span>
           </p>
         </div>
